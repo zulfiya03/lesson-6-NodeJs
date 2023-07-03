f54bbd4b10ed4ec1c65b2349c611941ee9ff7b0e
zulfiya <zulfiyaabdullaeva03@gmail.com>
2023-07-03 17:36:08 +0500

    next commit

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,29 @@
-import { NextFunction, Request } from "express";
+import { NextFunction, Request, Response } from "express";
+import authModels from "../models/auth.models";
 
-const registerMiddleware = async (
-    req: Request, 
-    res: Response,
-     )
+const register = async(req: Request, res:Response, next: NextFunction)=> {
+    const {name, surname, username, password} = req.body
+    const valid = authModels.registerScheme.validate(
+        {
+            name,
+            surname,
+            username, 
+            password,
+        },
+        {
+            abortEarly: false,
+        }
+    )
+
+    if (valid.error){
+        res.status(403).send({
+            message: valid.error.details.map((d)=>d.message).join(','),
+        })
+        return
+     }
+     next()
+}
+
+export default {
+    register,
+}
