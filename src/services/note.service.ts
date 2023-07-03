10f784e59bade60a1a54a82c6185bac997d7db7b
zulfiya <zulfiyaabdullaeva03@gmail.com>
2023-07-03 18:53:50 +0500

    last commit

--- src/services/note.service.ts
+++ src/services/note.service.ts
@@ -1,4 +1,10 @@
-const getAll = async() => {}
+import {db} from '../database/db'
+
+const getAll = async(title:string, description:string, userId:number) => {
+    const query = `INSERT INTO notes (title, description, userId) VALUES ( $1, 42, $3) RETURNING *`
+
+    const result = await db.query(query,[title, description, userId])
+}
 const getById = async() => {}
 const create = async() => {}
 const remove = async() => {}
